d2c82fbba0a1bd6770d4526f9b91ada1ae417871
alex <alexandre.sciacca@gmail.com>
2020-01-23 11:20:54 +0100

    first commit

---
+++ dist/VerticalSlider.js
@@ -0,0 +1,161 @@
+"use strict";
+
+(function () {
+  this.VerticalSlider = function (element, opts) {
+    var _this2 = this;
+
+    this.slides = new Array();
+    this.scrollLocked = false;
+    this.activeSlide = null;
+    this.wheelLocked = false;
+    var defaults = {
+      offset: 0,
+      delay: 2000,
+      duration: 1000,
+      animationDelay: 0
+    };
+    var scrollIndex = 0;
+    var observerOpts = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 1.0
+    };
+
+    if (opts === undefined) {
+      opts = {};
+    }
+
+    init.call(this, element, opts);
+    setDefaults.call(this, defaults, opts);
+    configure.call(this, opts);
+
+    var insersectionCallBack = function insersectionCallBack(element) {
+      if ((element.getAttribute('data-index') == 0 || element.getAttribute('data-index') == _this2.slides.length - 1) && !_this2.scrollLocked) {
+        var _this = _this2;
+        _this2.activeSlide = element;
+
+        _this2.activeSlide.classList.add('active');
+
+        scrollIndex = element.offsetTop;
+        toggleScrollState();
+
+        var wheelCallback = function wheelCallback(e) {
+          var delta = (e.deltaY || -e.wheelDelta || e.detail) >> 10 || 1;
+          if (_this.wheelLocked) return;
+          var nextSlide = null;
+
+          if (delta > 0) {
+            var nextIndex = parseInt(_this.activeSlide.getAttribute('data-index')) + 1;
+            var nextSlide = nextIndex === _this.slides.length ? null : _this.slides[nextIndex];
+          } else {
+            var nextIndex = parseInt(_this.activeSlide.getAttribute('data-index') - 1);
+            var nextSlide = nextIndex === -1 ? null : _this.slides[nextIndex];
+          }
+
+          if (null === nextSlide) {
+            document.removeEventListener('wheel', wheelCallback);
+            toggleScrollState();
+            return;
+          }
+
+          $('html,body').animate({
+            scrollTop: nextSlide.offsetTop + _this.offset
+          }, _this.duration);
+          scrollIndex = nextSlide.offsetTop;
+
+          _this.activeSlide.classList.remove('active');
+
+          setTimeout(function () {
+            nextSlide.classList.add('active');
+          }, _this.duration);
+          _this.activeSlide = nextSlide;
+          _this.wheelLocked = true;
+          setTimeout(function () {
+            _this.wheelLocked = false;
+          }, _this.delay);
+        };
+
+        document.addEventListener('wheel', wheelCallback);
+      }
+    };
+
+    var _self = this;
+
+    var scrollCallback = function scrollCallback(e) {
+      window.scrollTo(0, scrollIndex + _self.offset);
+    };
+
+    var scrollOffset = this.offset;
+
+    function toggleScrollState() {
+      if (!this.scrollLocked) {
+        this.wheelLocked = true;
+
+        var _self = this;
+
+        setTimeout(function () {
+          _self.wheelLocked;
+        }, 2000);
+        this.scrollLocked = true;
+        document.addEventListener('scroll', scrollCallback);
+      } else {
+        this.scrollLocked = false;
+        document.removeEventListener('scroll', scrollCallback);
+      }
+    }
+
+    function init(element, opts) {
+      var _this3 = this;
+
+      element.forEach(function (slide) {
+        _this3.slides.push(slide);
+      });
+      this.Observer = new IntersectionObserver(function (entries, observer) {
+        var _this4 = this;
+
+        entries.forEach(function (entry) {
+          if (entry.intersectionRatio == 1.0) {
+            var slide = entry.target;
+            insersectionCallBack.call(_this4, slide);
+          }
+        });
+      }, observerOpts);
+      var index = 0;
+      this.slides.forEach(function (slide) {
+        _this3.Observer.observe(slide);
+
+        slide.setAttribute('data-index', index++);
+      });
+    }
+    /* Define opts by defaults */
+
+
+    function setDefaults(defaults, opts) {
+      for (var key in defaults) {
+        if (!has.call(opts, key)) {
+          opts[key] = defaults[key];
+        }
+      }
+    }
+    /* object send has key */
+
+
+    function has(key) {
+      for (var _key in this) {
+        if (_key === key) return true;
+      }
+
+      return false;
+    }
+    /* Configure object with options */
+
+
+    function configure(opts) {
+      for (var key in opts) {
+        if (has.call(defaults, key)) {
+          this[key] = opts[key];
+        }
+      }
+    }
+  };
+})();
---
+++ src/VerticalSlider.js
@@ -0,0 +1,165 @@
+(function(){
+
+    this.VerticalSlider = function(element,opts) {
+      
+      this.slides = new Array();
+     
+      this.scrollLocked = false;
+      this.activeSlide = null;
+      
+      this.wheelLocked = false;
+      
+      
+      var defaults = {
+        offset: 0,
+        delay : 2000,
+        duration: 1000,
+        animationDelay: 0
+      }
+      
+      var scrollIndex = 0
+  
+      
+      var observerOpts = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 1.0
+      }
+      if(opts === undefined){
+        opts = {}
+      }
+      init.call(this,element,opts)
+      setDefaults.call(this,defaults,opts)
+      configure.call(this,opts);
+      
+      var insersectionCallBack = (element) => {
+        if((element.getAttribute('data-index') == 0 || element.getAttribute('data-index') == this.slides.length - 1) && !this.scrollLocked){
+          var _this = this;
+          
+          this.activeSlide = element;
+          this.activeSlide.classList.add('active')
+          scrollIndex = element.offsetTop;
+          toggleScrollState();
+          var wheelCallback = (e) => {
+            var delta = ((e.deltaY || -e.wheelDelta || e.detail) >> 10) || 1;
+            if(_this.wheelLocked)
+              return;
+            var nextSlide = null;
+            
+            if(delta > 0){
+              var nextIndex = parseInt(_this.activeSlide.getAttribute('data-index')) + 1
+              var nextSlide = (nextIndex === _this.slides.length) ? null : _this.slides[nextIndex]
+            } else{
+              var nextIndex = parseInt(_this.activeSlide.getAttribute('data-index') -1)
+              var nextSlide = (nextIndex === -1) ? null : _this.slides[nextIndex]
+            }
+            if(null === nextSlide){
+                document.removeEventListener('wheel',wheelCallback)
+                toggleScrollState()              
+              return
+            }
+            
+            
+            $('html,body').animate({
+              scrollTop: nextSlide.offsetTop + _this.offset
+            },_this.duration)
+         
+            
+  
+            scrollIndex = nextSlide.offsetTop
+            _this.activeSlide.classList.remove('active')
+            
+            setTimeout(function(){
+              nextSlide.classList.add('active')  
+            },_this.duration)
+            _this.activeSlide = nextSlide
+          
+            _this.wheelLocked = true
+            setTimeout(function(){
+              _this.wheelLocked = false
+              
+            },_this.delay)
+          }
+          
+          document.addEventListener('wheel',wheelCallback)
+        }
+      }    
+      var _self = this;
+      var scrollCallback = (e) => {
+        
+        window.scrollTo(0,scrollIndex + _self.offset)
+      }
+     
+     
+      
+      var scrollOffset = this.offset
+      
+      
+      function toggleScrollState(){
+   
+        if(!this.scrollLocked){
+          this.wheelLocked = true;
+          var _self = this;
+          setTimeout(() => {
+            _self.wheelLocked
+          },2000)
+          this.scrollLocked = true
+          document.addEventListener('scroll',scrollCallback)
+        } else{
+          this.scrollLocked = false
+          document.removeEventListener('scroll',scrollCallback)
+        }
+      }
+      
+      function init(element,opts){ 
+        element.forEach((slide) => {
+          this.slides.push(slide)
+        })
+        this.Observer = new IntersectionObserver(function(entries,observer){
+          entries.forEach(entry => {
+            if(entry.intersectionRatio == 1.0){
+              var slide = entry.target
+              insersectionCallBack.call(this,slide)
+  
+            }
+          })
+        },observerOpts)
+        var index = 0 
+        this.slides.forEach((slide) => {
+          this.Observer.observe(slide)
+          slide.setAttribute('data-index',index++)
+          
+        })
+        
+      }
+      
+      /* Define opts by defaults */
+      function setDefaults(defaults,opts){
+        for(var key in defaults){
+          if(!has.call(opts,key)){
+            opts[key] = defaults[key]
+          }
+        }
+      }
+      /* object send has key */
+      function has(key){
+        for(var _key in this){
+          if(_key === key)
+            return true;
+        }
+        return false;
+      }
+       /* Configure object with options */
+      function configure(opts){ 
+        
+        for(var key in opts){
+          
+          if(has.call(defaults,key)){
+             this[key] = opts[key]                  
+          }
+        }
+      }
+       
+    }
+   
+  })()  
